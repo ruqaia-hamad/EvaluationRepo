488c70fd366007461cbc3c8a45a4edcee0d828f5
Ruqaia Hamed <59163691+ruqaia-hamad@users.noreply.github.com>
2022-12-06 16:55:22 +0400

    adding option to search

--- src/SearchFile.java
+++ src/SearchFile.java
@@ -9,65 +9,112 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.font.PDSimpleFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
+import org.apache.pdfbox.text.PDFTextStripper;
+
 public class SearchFile {
 
 	public static void Search() throws IOException {
 		boolean isExit = true;
 
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Please choose form where you want Search: " + "\n");
+		System.out.print("1-TXT" + "\n");
+		System.out.print("2-PDF " + "\n");
+		Integer choose = sc.nextInt();
+		if (choose == 1) {
+			File file = new File("C:\\Users\\user015\\eclipse-workspace\\EvaluationTaskRaqiya\\writefile2.txt");
+			List<String> wordsList = new ArrayList<String>();
+			String[] words = null;
 
-		File file = new File("C:\\Users\\user015\\eclipse-workspace\\EvaluationTaskRaqiya\\writefile2.txt");
-		List<String> wordsList = new ArrayList<>();
-		String[] words = null;
+			String userinput = null;
+			System.out.print("Please enter the words you want to Search | writ EXIT to stop" + "\n");
 
-		String userinput = null;
-		System.out.print("Please enter the words you want to Search | writ EXIT to stop" + "\n");
+			while (sc.hasNext()) {
+				userinput = sc.nextLine();
+				if (userinput.equalsIgnoreCase("EXIT")) {
+					break;
+				}
 
-		while (sc.hasNext()) {
-			userinput = sc.nextLine();
-			if (userinput.equalsIgnoreCase("EXIT")) {
-				break;
+				wordsList.add(userinput);
 			}
 
-			wordsList.add(userinput);
-		}
+			for (String n : wordsList) {
 
-		for (String n : wordsList) {
+				FileReader fileReader = new FileReader(file);
 
-			FileReader fileReader = new FileReader(file);
+				BufferedReader br = new BufferedReader(fileReader);
+				String x;
+				int count = 0;
+				while ((x = br.readLine()) != null) {
+					words = x.split(" ");
 
-			BufferedReader br = new BufferedReader(fileReader);
-			String x;
-			int count = 0;
-			while ((x = br.readLine()) != null) {
-				words = x.split(" ");
-
-				for (String word : words) {
-					if (word.contains(n)) {
+					for (String word : words) {
+						if (word.contains(n)) {
 
 //						System.out.println("the word is " + n);
-						count++;
+							count++;
+
+						}
 
 					}
+				}
 
+				if (count > 1) {
+
+					System.out.println(n + " " + "This word is present for " + count + " Times in the file");
+				} else if (count == 1) {
+
+					System.out.println(n + "" + "This word is unique ");
+
+				}
+
+				else {
+					System.out.println(n + " " + "This word is not in the file");
+				}
+
+				fileReader.close();
+
+			}
+		} else if (choose == 2) {
+
+			List<String> wordsListpdf = new ArrayList<String>();
+			File file = new File("C:\\Users\\user015\\eclipse-workspace\\EvaluationTaskRaqiya\\random.pdf");
+
+			PDDocument document = PDDocument.load(file);
+		
+		
+//			System.out.println(br);
+			String[] words = null;
+
+			String userinput2 = null;
+			System.out.print("Please enter the words you want to Search | writ EXIT to stop" + "\n");
+			int count = 0;
+			while (sc.hasNext()) {
+				userinput2 = sc.nextLine();
+				if (userinput2.equalsIgnoreCase("EXIT")) {
+					break;
+				}
+
+				wordsListpdf.add(userinput2);
+			}
+			for (int i = 0; i < document.getNumberOfPages(); i++) {
+			    PDPage page = document.getPage(i);
+				PDFTextStripper ps = new PDFTextStripper();
+				String br = ps.getText(document);
+			for (String n : wordsListpdf) {
+				if (br.contains(n)) {
+				     System.out.println("Found word '" + n+ "' in page " + (i + 1));
+				
 				}
 			}
 
-			if (count > 1) {
-
-				System.out.println(n + " " + "This word is present for " + count + " Times in the file");
-			} else if (count == 1) {
-
-				System.out.println(n + "" + "This word is unique ");
-
-			}
-
-			else {
-				System.out.println(n + " " + "This word is not in the file");
-			}
-
-			fileReader.close();
+	
 
 		}
+			document.close();}
 	}
 }
